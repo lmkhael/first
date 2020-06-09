13be182c043f845919149872363e81504fa2d481
lmkhael <loukas.mkhael@gmail.com>
2020-06-09 17:44:44 +0000

    double working

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-name = input('What is your name?\n')
-print('Hi, %s.' % name)
+number = input('Give me an int\n')
+print('your number is: %s.' % number)
+
+def double (x):
+  return (x) *2
+
+monchiffre = int(number)
+
+doublechiffre = double(monchiffre)
+print ("the double of your number is: " + str(doublechiffre)) 
