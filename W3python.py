1d1aeaad631fba0e576940ecdfc52c60c52e1663
Loukas <loukas.mkhael@gmail.com>
2020-07-14 13:12:18 +0200

    added two new files: playing with w3python and flask

---
+++ W3python.py
@@ -0,0 +1,6 @@
+i = 0
+while i < 100:
+    i += 1
+    if i == 3:
+      continue
+    print(i)
--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-number = input('Give me an int\n')
-print('your number is: %s.' % number)
 
-def double (x):
-  return (x) *2
+def add_one(x):
+    return x + 1
 
-monchiffre = int(number)
 
-doublechiffre = double(monchiffre)
-print ("the double of your number is: " + str(doublechiffre)) 
+def divide(x, y):
+    return x / y
+
+
+def double(x):
+    return x * 2
+
+
+def multiply(x, y):
+    return float(x) * float(y)
+
+
+first = input('Give me a first number\n')
+second = input('Give me a second number\n')
+
+result = multiply(first, second)
+print(result)
+
+
+# print('your number is: %s.' % number)
+
+#monchiffre = float(number)
+
+#doublechiffre = double(monchiffre)
+#print("the double of your number is: " + str(doublechiffre))
